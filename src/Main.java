9960acb492ee70ef3c66e61c7a2c9719670baeb5
Manihaa <manihaa@nycstudents.net>
2025-11-20 10:19:51 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        ArrayList<String> lines = getFileData("src/data");
+        ArrayList<String> lines = getFileData("src/input.txt");
 
         int partOneAnswer = 0;
         int partTwoAnswer = 0;
         for (int i = 0; i < lines.size(); i++) {
             partOneAnswer += getPartOneNumber(lines.get(i));
-            partTwoAnswer += getPartTwoNumber(lines.get(i));
+            //partTwoAnswer += getPartTwoNumber(lines.get(i));
         }
 
         System.out.println("Part one answer: " + partOneAnswer);
@@ -23,9 +23,18 @@ public class Main {
     }
 
     public static int getPartOneNumber(String line) {
-        // do part 1
-        // find the first digit and last digit of the String
-        // return the number put together
+        int len = line.length();
+        String nums = "";
+        int[] digits = {0,1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i < len; i++){
+            for (int k = 0; k < digits.length; k++) {
+                if (line.substring(i, i + 1).equals(digits[k])){
+                    nums += line.substring(i, i+1);
+                }
+            }
+        }
+        String newNum = nums.substring(0, 1) + nums.substring(nums.length() - 1, nums.length());
+        int final_num = newNum;
         return 0;
     }
 
