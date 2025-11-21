62301765170a5fc4b349d70ce3ce5c3a63bd7273
ManihaAkhter <manihaa@nycstudents.net>
2025-11-21 10:19:33 -0500

    it works now but it may not be correct

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,35 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         ArrayList<String> lines = getFileData("src/input.txt");
-
         int partOneAnswer = 0;
-        int partTwoAnswer = 0;
+        //int partTwoAnswer = 0;
+
         for (int i = 0; i < lines.size(); i++) {
             partOneAnswer += getPartOneNumber(lines.get(i));
             //partTwoAnswer += getPartTwoNumber(lines.get(i));
         }
 
         System.out.println("Part one answer: " + partOneAnswer);
-        System.out.println("Part two answer: " + partTwoAnswer);
+        //System.out.println("Part two answer: " + partTwoAnswer);
     }
 
     public static int getPartOneNumber(String line) {
         int len = line.length();
         String nums = "";
-        int[] digits = {0,1,2,3,4,5,6,7,8,9};
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
         for (int i = 0; i < len; i++){
             for (int k = 0; k < digits.length; k++) {
                 if (line.substring(i, i + 1).equals(digits[k])){
-                    nums += line.substring(i, i+1);
+                    nums = nums + line.substring(i, i+1);
                 }
             }
+            System.out.println(nums);
         }
-        String newNum = nums.substring(0, 1) + nums.substring(nums.length() - 1, nums.length());
-        int final_num = newNum;
-        return 0;
+
+        int newNum = Integer.parseInt(nums.substring(0, 1) + nums.substring(nums.length() - 1, nums.length()));
+        //int final_num = newNum;
+        return newNum;
     }
 
     public static int getPartTwoNumber(String line) {
