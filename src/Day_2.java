f9d59745eb677af6426af8af987c33bc2a4fe508
ManihaAkhter <manihaa@nycstudents.net>
2025-11-25 10:19:14 -0500

    this is hard

--- src/Day_2.java
+++ src/Day_2.java
@@ -22,7 +22,27 @@ public class Day_2 {
     public static int partOne(String game){
         int colonIndex = game.indexOf(":");
         int id = Integer.parseInt(game.substring(5, colonIndex));
-        return id;
+        boolean boo = true;
+        int semiIndex = game.indexOf(";");
+        int k = game.indexOf(":") + 1;
+
+        int tempR = 0;
+        int tempG = 0;
+        int tempB = 0;
+        while (game.contains(";")){
+            String tempGame = game.substring(k, semiIndex);
+
+            k = semiIndex + 1;
+            semiIndex = game.indexOf(";");
+            if (semiIndex < 0){
+                semiIndex =
+            }
+        }
+
+        if (boo){
+            return id;
+        }
+        return 0;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
