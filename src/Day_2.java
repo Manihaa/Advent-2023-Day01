2ebc29f374d3e16dbe8bf5bccff17cad71e3b667
ManihaAkhter <manihaa@nycstudents.net>
2025-12-01 10:16:55 -0500

    completed day 2

--- src/Day_2.java
+++ src/Day_2.java
@@ -9,34 +9,62 @@ public class Day_2 {
     private static int b = 14;
 
     public static void main(String[] args){
-        ArrayList<String> lines = getFileData("src/input.txt");
+        ArrayList<String> lines = getFileData("src/input_2.txt");
         int total = 0;
 
         for (int i = 0; i < lines.size(); i++){
-            //total += partOne(lines.get(i));
+            total += partOne(lines.get(i));
         }
 
-        //System.out.println(total);
+        System.out.println(total);
     }
 
     public static int partOne(String game){
+        boolean boo = true;
         int colonIndex = game.indexOf(":");
         int id = Integer.parseInt(game.substring(5, colonIndex));
-        boolean boo = true;
-        int semiIndex = game.indexOf(";");
-        int k = game.indexOf(":") + 1;
+        game = game.replace(";", ",");
+        game = game.substring(colonIndex + 2);
+        int idx = game.indexOf(",");
 
         int tempR = 0;
         int tempG = 0;
         int tempB = 0;
-        while (game.contains(";")){
-            String tempGame = game.substring(k, semiIndex);
 
-            k = semiIndex + 1;
-            semiIndex = game.indexOf(";");
-            if (semiIndex < 0){
-                semiIndex =
+        while(idx > -1 && boo){
+            String let = game.substring(game.indexOf(" ") + 1, game.indexOf(" ") + 2);
+            int num = Integer.parseInt(game.substring(0, game.indexOf(" ")));
+            if (let.equals("r")){
+                tempR = num;
+            }else if (let.equals("g")){
+                tempG = num;
+            }else if (let.equals("b")){
+                tempB = num;
             }
+
+            if (tempR > r || tempG > g || tempB > b){
+                boo = false;
+            }
+
+            tempR = 0;
+            tempG = 0;
+            tempB = 0;
+            game = game.substring(idx + 2);
+            idx = game.indexOf(",");
+        }
+        // one last iteration
+        String let = game.substring(game.indexOf(" ") + 1, game.indexOf(" ") + 2);
+        int num = Integer.parseInt(game.substring(0, game.indexOf(" ")));
+        if (let.equals("r")){
+            tempR = num;
+        }else if (let.equals("g")){
+            tempG = num;
+        }else if (let.equals("b")){
+            tempB = num;
+        }
+
+        if (tempR > r || tempG > g || tempB > b){
+            boo = false;
         }
 
         if (boo){
